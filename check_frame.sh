8a477cff32276fe42d6dc2e518a45b04fb89a15a
Gabriel da Hora <66845152+gdahora@users.noreply.github.com>
2023-02-14 16:50:15 -0700

    Delete check_frame.sh

--- check_frame.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {1..5}
-do
-    cd r$i/TRAJ/
-    cpptraj -p p00-noWater.parm7 -y TRAJ_J25_noWater.nc -tl
-    cpptraj -p p00-noWater.parm7 -y 1to8_6nm/DISTANCES_filtered_2to6nm.nc -tl
-    cd ../../
-done
